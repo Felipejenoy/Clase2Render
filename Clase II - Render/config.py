776177f978076cc2eb92375000906695fba84fe9
Felipejenoy <johanordonez@unicauca.edu.co>
2024-10-16 14:46:43 -0500

    Se le añadio una cadena de conexión

--- Clase II - Render/config.py
+++ Clase II - Render/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://testdb_fwms_user:MclrHbdtGttpwx7NvXqN286mtg4LfEgD@dpg-crue83jv2p9s73epvrf0-a/testdb_fwms'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://clase2_render_user:PvTlhNNHg4zDa3WlNPbHvEv8Uq7GEhgV@dpg-cs81fgo8fa8c73bin7lg-a/clase2_render'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
